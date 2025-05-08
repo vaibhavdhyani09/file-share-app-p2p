68cebf91cea69fd6297f0f79856eb93eba866c53
Vaibhav <vd090904@gmail.com>
2025-05-08 19:55:33 +0530

    Duplicate detection

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import threading
 import os
 import customtkinter as ctk
 from zeroconf import Zeroconf, ServiceInfo
+from tkinter import messagebox
 
 ctk.set_appearance_mode("system")
 ctk.set_default_color_theme("blue")
@@ -56,6 +57,17 @@ def start_server():
             filename = os.path.basename(filename)
             filenamelabel.configure(text=f"Receiving: {filename}")
             filepath = os.path.join(save, filename)
+            if os.path.exists(filepath):
+                base, ext = os.path.splitext(filename)
+                count = 1
+                new_filename = f"{base}_copy{count}{ext}"
+                while os.path.exists(os.path.join(save, new_filename)):
+                    count += 1
+                    new_filename = f"{base}_copy{count}{ext}"
+                filepath = os.path.join(save, new_filename)
+                # print(f"⚠ File already exists. Saving as: {new_filename}")
+                messagebox.showinfo("Copy exists!", f"File exists. Saved as: {new_filename}")
+
             filesize = int(filesize)
 
             received_size = 0
